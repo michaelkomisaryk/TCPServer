6b01d579827511566e44269c4e44fe437e7e0f45
jchordan-ctrl <jchordan@gmail.com>
2026-08-17 19:33:08 +0300

    task 2

--- server.py
+++ server.py
@@ -2,15 +2,28 @@ import socket
 
 def run_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    HOST = '127.0.0.1'
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    HOST = 'localhost'
     PORT = 8080
     server_socket.bind((HOST, PORT))
     server_socket.listen(1)
     print(f'Server start working on {HOST}:{PORT} ')
-    client_socket, client_address = server_socket.accept()
-    print(f"Connected by {client_address}")
-    client_socket.close()
-    server_socket.close()
+    while True:
+        client_socket, client_address = server_socket.accept()
+        request_bytes = client_socket.recv(1024)
+        if request_bytes:
+            request_text = request_bytes.decode('utf-8')
+            print(f'Revived HTTP request: {request_text}')
+
+            http_response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain; charset=utf-8\r\n"
+                "Connection: close\r\n\r\n"
+                "Hello from raw socket server!"
+            )
+            client_socket.sendall(http_response.encode('utf-8'))
+        client_socket.close()
+
 
 
 if __name__ == '__main__':
